4be0f814b6e523fcec684d21ed59c109633bcf45
drogonj <108951407+drogonj@users.noreply.github.com>
2024-10-13 16:41:12 +0200

    add commentary

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main() {
 	char bjr[9] = {0};
 	char ft_bjr[9] = {0};
 
-	{
+	{	// ft_strcpy
 		printf("%s/////   ft_strcpy   /////%s\n", MG, R);
 		strcpy(bjr, "bonjour");
 		printf("%s\n", bjr);
@@ -33,12 +33,12 @@ int main() {
 		printf("%s\n", ft_bjr);
 
 	}
-	{	
+	{	// ft_strlen
 		printf("%s/////   ft_strlen   /////%s\n", MG, R);
 		printf("strlen: %lu\n", strlen(bjr));
 		printf("ft_strlen: %lu\n", ft_strlen(bjr));
 	{
-	}
+	}	// ft_write
 		printf("%s/////   ft_write   /////%s\n", MG, R);
 		printf("%swrite%s\n", BL, R);
 		printf("\nreturn: %ld\n", write(0, bjr, strlen(bjr)));
@@ -47,14 +47,14 @@ int main() {
 		printf("\nreturn: %ld\n", ft_write(0, bjr, strlen(bjr)));
 		perror("errno");
 	}
-	{	
+	{	// ft_strcmp
 		printf("%s/////   ft_strcmp   /////%s\n", MG, R);
 		char a[] = "aaa";
 		char b[] = "aab";
 		printf("strcmp: %d\n", strcmp(a, b));
 		printf("ft_strcmp: %d\n", ft_strcmp(a, b));
 	}
-	{
+	{	// ft_strdup
 		printf("%s/////   ft_strdup   /////%s\n", MG, R);
 		printf("%soriginal: %p\n%s", BL, bjr, R);
 		char * c = strdup("bonjour");	
@@ -68,7 +68,7 @@ int main() {
 		free(c);
 		free(d);
 	}
-	{
+	{	// ft_read
 		printf("%s/////   ft_read   /////%s\n", MG, R);
 		int fd = open("./.gitignore", O_RDONLY);
 		char abuff[7] = {0};
